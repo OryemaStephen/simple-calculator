1e4d3252457702d28c3f1e6e1b9e84e131aa1922
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-04-19 21:44:26 +0300

    Modified view and functionality

--- app.js
+++ app.js
@@ -1,14 +1,79 @@
+const display = document.getElementById('display');
+const userInput = document.getElementById('user-input');
+const button = document.querySelectorAll('.button');
+const deleteButton = document.querySelector('.del');
+const clearButton = document.querySelector('.ac');
+const percentButton = document.querySelector('.percent');
+const equalButton = document.querySelector('.equal');
 
-function inputValue(val){
-    document.getElementById('display').value = val;
+function getUserInput(){
+    button.forEach((button)=>{
+        button.addEventListener('click', (event)=>{
+            let input = event.target.value;
+            userInput.value += input;
+        })
+    })     
 }
 
-function input(v){
-    document.getElementById('display').value+=v;
+function calculate() {
+    equalButton.addEventListener('click', () => {
+        const input = userInput.value;
+        let numbers = input.match(/\d+/g).map(parseFloat); 
+        let operators = input.match(/[^\d]+/g);
+
+        let result = parseFloat(numbers[0]); 
+
+        for (let i = 1; i < numbers.length; i++) {
+            const operator = operators[i - 1]; 
+            const number = numbers[i];
+
+            switch (operator) {
+                case '+':
+                    result += number;
+                    break;
+                case '-':
+                    result -= number;
+                    break;
+                case '*':
+                    result *= number;
+                    break;
+                case '/':
+                    if (number === 0) {
+                    alert("Error");
+                    return; 
+                    }
+                    result /= number;
+                    break;
+                default:
+                    alert("Error");
+                    return; 
+            }
+        }
+        display.value = result;
+    });
 }
 
-function calculate(){
-    let input = document.getElementById('display').value;
-    let y =eval(input);
-    document.getElementById('display').value = y;
+function deleteInput(){
+    deleteButton.addEventListener('click', ()=>{
+        userInput.value = userInput.value.slice(0,-1);
+    })
 }
+
+function clearInput(){
+    clearButton.addEventListener('click', ()=>{
+        userInput.value = '';
+    })
+}
+
+function getPercent(){
+    percentButton.addEventListener('click', ()=>{
+        display.value = Number(userInput.value)/100;
+
+    })
+}
+
+getUserInput();
+calculate();
+getPercent();
+deleteInput();
+clearInput();
